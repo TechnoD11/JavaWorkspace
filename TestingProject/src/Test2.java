a6521bc55b2206f79b88abdb58f79e00750540ec
Derek Wider <technoD@outlook.com>
2014-11-08 20:43:12 -0500

    commit 5

--- TestingProject/src/Test2.java
+++ TestingProject/src/Test2.java
@@ -2,8 +2,26 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-		String str = "hello";
-		str.
+		boolean resp = gHappy("gg");
+		System.out.println(resp);
 	}
+	public static boolean gHappy(String str) {
+		  boolean resp = false;
+		  char first, second, third;
+		  for(int i = 0; (i+2) < str.length(); i++){
+		    first = str.charAt(i);
+		    second = str.charAt(i+1);
+		    third = str.charAt(i+2);
+		    if(second == 'g'){
+		      if(first == 'g' || third == 'g'){
+		        resp = true;
+		      }
+		      else{
+		        resp = false;
+		      }
+		    }
+		  }
+		  return resp;
+		}
 
 }
