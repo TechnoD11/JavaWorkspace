52a77283c77b5558c478a58cb2f1d82c75b2726a
Derek Wider <technoD@outlook.com>
2014-11-24 22:51:05 -0500

    lab 8.2

--- ProgramAssign4/src/primeCalculator.java
+++ ProgramAssign4/src/primeCalculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * 
  * @author Derek Wider
  * Programming Assignment 4
- *Prime Generator. Calculates the number of primes between 2 and a user-inputted value, inclusive. Prints the number of checks it performed and the total number of primes generated.
+ *Prime Generator. Calculates the number of primes between 2 and a user-inputed value, inclusive. Prints the number of checks it performed and the total number of primes generated.
  */
 public class primeCalculator {
 	public static void main(String[] args){
@@ -16,18 +16,21 @@ public class primeCalculator {
 		int testCounter = 0;
 		int rowCounter = 0; //primitives to hold the data
 		boolean isPrime = true;
-		for(int i = 2; i <= primeLimit; i++){ //while less than the number to calculate to
+		System.out.print(2 + "\t"); //print out 2, since its prime and the algorithm can be more efficient without calculating it
+		rowCounter++;
+		numberOfPrimes++;
+		for(int i = 3; i <= primeLimit; i+=2){ //while less than the number to calculate to
 			isPrime = true;
-			for(int j = 2; (j <= (i/2)) && isPrime; j++){ //while j is less than half of i and i is still prime (yet) 
+			for(int j = 3; (j <= (i/2)) && isPrime; j+=2){ //while j is less than half of i and i is still prime (yet) 
 				if(i % j == 0){ //if its not prime, set prime to false
 					isPrime = false;
-					testCounter++;
 				}
+				testCounter++;
 			}
 			if(isPrime){ //if its prime
 				numberOfPrimes++; //increase number of primes
 				if(rowCounter != ROW_LIMIT){ //if it hasn't printed ROW_LIMIT numbers yet
-					System.out.print(i + " "); //print i, the prime number
+					System.out.print(i + "\t"); //print i, the prime number
 					rowCounter++; //increment down row
 				}
 				else{
