eef8c5e3a0acd311132fd68e86a9f5b7419ecaf3
Derek Wider <technoD@outlook.com>
2014-12-16 22:40:41 -0500

    8.4 rows and columns work

--- Lab8.4/src/Sudoku.java
+++ Lab8.4/src/Sudoku.java
@@ -39,8 +39,72 @@ public class Sudoku
    }
 
    public boolean isValidSudoku(){
-	   boolean isValid = true;
-	   
+	   boolean isValid = false;
+	   if(rowsAreLatin() && colsAreLatin()){
+		  isValid = true;
+	   }
+	   else{
+		   isValid = false;
+	   }
 	   return isValid;
+	   
+   }
+   public boolean rowsAreLatin(){
+	   boolean[] isFound = new boolean[9];
+	   for(int i = 0; i < isFound.length; i++){
+		   isFound[i] = false;
+	   }
+	   for(int i = 0; i < x.length; i++){
+		   for(int k = 0; k < x[0].length; k++){
+			   isFound[(Integer.parseInt(x[i][k])) - 1] = true;
+		   }
+		   for(int j = 0; j < x[0].length; j++){
+			   if(!isFound[j]){
+				   return false;
+			   }
+		   }
+		   for(int k = 0; k < isFound.length; k++){
+			   isFound[k] = false;
+		   }
+	   }
+	   return true;
+   }
+   public boolean colsAreLatin(){
+	   boolean[] isFound = new boolean[9];
+	   for(int i = 0; i < isFound.length; i++){
+		   isFound[i] = false;
+	   }
+	   for(int i = 0; i < x.length; i++){
+		   for(int k = 0; k < x[0].length; k++){
+			   isFound[(Integer.parseInt(x[k][i])) - 1] = true;
+		   }
+		   for(int j = 0; j < x[0].length; j++){
+			   if(!isFound[j]){
+				   return false;
+			   }
+		   }
+		   for(int k = 0; k < isFound.length; k++){
+			   isFound[k] = false;
+		   }
+	   }
+	   return true;
+   }
+   public boolean goodSubsquares(){
+	   boolean isValid = true;
+	   boolean isFound[][] = new boolean[3][3];
+	   for(int i = 0; i < isFound.length; i++){
+		   for(int j = 0; j < isFound[0].length; j++){
+			   isFound[i][j] = false;
+		   }
+	   }
+	   for(int i = 3; i <= x.length; i += 3){
+		   for(int j = (i - 1); j >= (i - 3); j--){
+			   for(int k = (i - 1); k >= (i - 3); k--){
+				   
+			   }
+		   }
+	   }
+	   return isValid;
+
    }
 }
