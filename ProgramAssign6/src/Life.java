653fb9bd81a84a373e489715ead4d4324f5ed6c8
Derek Wider <technoD@outlook.com>
2015-01-08 21:03:48 -0500

    finished game of life
    all done!

--- ProgramAssign6/src/Life.java
+++ ProgramAssign6/src/Life.java
@@ -1,75 +1,71 @@
+/**
+ * @author Derek Wider
+ * Programming Assignment 6
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
 
-
-public class Life {
-	private int theRows;
-	private int theColumns;
+public class Life { 
 	private int rows;
-	private int columns;
+	private int columns; //instance variables of the life class
 	private boolean theGrid[][];
 	private Random rand;
 	private Scanner inData;
 	private File inFile;
-	public Life(int rows, int columns){
+	
+	public Life(){ //constructor, only needed to create new instance of the random class
+		rand = new Random();
+	}
+	public void createGrid(int rows, int columns){ //creates the grid and clears it
 		theGrid = new boolean[rows][columns];
 		this.rows = rows;
-		theRows = rows;
-		theColumns = columns;
 		this.columns = columns;
-		rand = new Random();
 		clearGrid();
 	}
-	public void clearGrid(){
-		for(int i = 0; i < rows; i++){
-			for(int j = 0; j < columns; j++){
-				theGrid[i][j] = false;
+	public void clearGrid(){ //iterates through the grid and sets all members to false
+		for(int i = 0; i < rows; i++){//rows
+			for(int j = 0; j < columns; j++){//columns
+				theGrid[i][j] = false; //set to false
 			}
 		}
 	}
-	public void randomBuild(){
-		clearGrid();
-		for(int i = 0; i < theGrid.length; i++){
-			for(int j = 0; j < theGrid[0].length; j++)
-				if(rand.nextInt(10) < 3){
+	public void randomBuild(){ //builds a random game of life grid using the random class
+		clearGrid();//clear grid
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < columns; j++)
+				if(rand.nextInt(10) < 3){ //generate integer between 0-9, if less than 3, set array element to true
 					theGrid[i][j] = true;
 				}
 		}
 	}
-	private void resetBounds(){
-		rows = theRows;
-		columns = theColumns;
-	}
-	public void fileInput(String directory, String fileName) throws FileNotFoundException{
+	public void fileInput(String directory, String fileName) throws FileNotFoundException{ //reads in file to build game board from
+		inFile = new File(directory, fileName); 
+		inData = new Scanner(inFile); //read in file from parameter variables of directory and fileName
+		String lineData = inData.next(); //get first data line
+		columns = lineData.length();//set column size of grid
+		rows = 0;
+		while(inData.hasNext()){
+			rows++;
+			inData.next(); //go through grid, when end is reached, row count is reached
+		}
+		createGrid(rows, columns); //create the grid with the gathered data
 		clearGrid();
-		inFile = new File(directory, fileName); //scan in a paint data file
-		inData = new Scanner(inFile);
-		String lineData = inData.next();
-		int columnCounter = 0;
-		boolean contains = true;
-		for(int i = 0; i < columns && contains; i++){
-			for(int j = 0; j < lineData.length(); j++){
+		inData.close();
+		inFile = new File(directory, fileName); 
+		inData = new Scanner(inFile); //reset instance variables to the same file from the beginning
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < columns; j++){ //iterate through the file
 				if(lineData.charAt(j) == '.'){
-					theGrid[i][j] = false;
+					theGrid[i][j] = false; //read the characters, if empty, set to false
 				}
-				else{
+				else{ //else, true
 					theGrid[i][j] = true;					
 				}
-				columnCounter++;
 			}
-			try{
-				lineData = inData.next();
-			}catch (NoSuchElementException e){
-				contains = false;
-				columns = columnCounter;
-				rows = i + 1;
-			}
-			columnCounter = 0;
+			lineData = inData.next(); //read the next line
 		}
-		inData.close();
 	}
 	public void initialGrid(){
 		int r = rows/3;					// Set up a third of the way down
@@ -82,37 +78,37 @@ public class Life {
 		theGrid[r + 1][c + 6] = theGrid[r + 1][c + 7] = true;	//middle 2 right
 		theGrid[r + 2][c + 3] = theGrid[r + 2][c + 8] = true;	//bottom row
 	}
-	public void next(){
-		boolean[][] newBoard = new boolean[rows][columns];
+	public void next(){ //creates the next generation based on the game of life rules
+		boolean[][] newBoard = new boolean[rows][columns]; //create a temporary board, same size as real game board
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
-				newBoard[i][j] = false;
+				newBoard[i][j] = false; //set all to false
 			}
 		}
 		int cellCounter = 0;
 		for (int i = (rows - 1); i >= 0; i--) {
-			for (int j = (columns - 1); j >= 0; j--) {
+			for (int j = (columns - 1); j >= 0; j--) { //iterate through the game board
 				for (int k = (i - 1); k < (i + 2); k++) {
-					for (int m = (j - 1); m < (j + 2); m++) {
+					for (int m = (j - 1); m < (j + 2); m++) {//iterate around the testable element
 						if (!(k == i && m == j)) {
-							if (!((k < 0 || k > (rows - 1)) || (m < 0 || m > (columns - 1)))) {
+							if (!((k < 0 || k > (rows - 1)) || (m < 0 || m > (columns - 1)))) { //if not out of bounds and not the original point
 								if (theGrid[k][m]) {
-									cellCounter++;
+									cellCounter++; //if a cell is alive at the surrounding point, increase the counter
 								}
 							}
 						}
 					}
 				}
-				if (cellCounter == 3 || cellCounter == 2 && theGrid[i][j]) {
-					newBoard[i][j] = true;
+				if (cellCounter == 3 || cellCounter == 2 && theGrid[i][j]) { //if meets the criteria
+					newBoard[i][j] = true; //create on new board
 				}
-				cellCounter = 0;
+				cellCounter = 0; //reset cell counter
 			}
 		}
 		//transpose onto new grid
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
-				if(newBoard[i][j]){
+				if(newBoard[i][j]){ //read temp board, set temp board data on real game board
 					theGrid[i][j] = true;
 				}
 				else{
@@ -121,19 +117,19 @@ public class Life {
 			}
 		}
 	}
-	public String toString(){
+	public String toString(){ //creates a string representation of the board
 		String board = "";
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
-				if(theGrid[i][j]){
+				if(theGrid[i][j]){ //iterate through, if cell is alive, mark with *
 					board += "*";
 				}
 				else{
-					board += " ";
+					board += " "; //else, [space]
 				}
 			}
-			board += "\n";
+			board += "\n";//create a new line, to create the board
 		}
-		return board;
+		return board; //return the board
 	}
 }
