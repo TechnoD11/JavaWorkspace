bab364c1ccdb2edfbf6f2d2377dcc8b13aeafd2f
Derek Wider <technoD@outlook.com>
2015-03-17 22:30:28 -0400

    continued critter exercises

--- GridWorldProj2015/src/critterCrab/ChameleonKid.java
+++ GridWorldProj2015/src/critterCrab/ChameleonKid.java
@@ -8,10 +8,18 @@ import java.util.ArrayList;
 public class ChameleonKid extends ChameleonCritter{
 
 	public void processActors(ArrayList<Actor> actors){
-		Actor north = getGrid().get(this.getLocation().getAdjacentLocation(Location.NORTH));
-		Actor south = getGrid().get(this.getLocation().getAdjacentLocation(Location.SOUTH));
-		if(north == null && south == null){
-			super.darken();
+		for(int i = actors.size()-1; i >= 0; i--){
+			int direction = this.getDirection() + 180;
+			if(direction > 359){
+				direction = this.getDirection() - 180;
+			}
+			if(this.getLocation().getDirectionToward(actors.get(i).getLocation()) != this.getDirection()){
+				actors.remove(i);
+			}
+			else if(this.getLocation().getDirectionToward(actors.get(i).getLocation()) != direction){
+				actors.remove(i);
+			}
 		}
+		super.processActors(actors);
 	}
 }
