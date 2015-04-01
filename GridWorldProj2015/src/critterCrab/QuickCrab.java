58504cb0ac5d5b4120f9dd9ae7b25d198eac2d36
TechnoD11 <technoD@outlook.com>
2015-03-31 20:00:10 -0400

    new critter changes (still not working)

--- GridWorldProj2015/src/critterCrab/QuickCrab.java
+++ GridWorldProj2015/src/critterCrab/QuickCrab.java
@@ -12,21 +12,22 @@ public class QuickCrab extends CrabCritter{
 	
 	public ArrayList<Location> getMoveLocations() { //creates array of move locations
 		ArrayList<Location> locations = new ArrayList<Location>();
-		int[] directions = { this.getDirection()-90, this.getDirection()+90 }; //gets left and right directions to use in array
+		int[] directions = {this.getDirection() - 90, this.getDirection() + 90}; //gets left and right directions to use in array
 		for (Location location : getLocationsInDirections(directions)) { //iterate through the getLocationsInDirections arraylist
 			/*if (getGrid().get(location) == null){ //if the locations directly left and right are valid
 				locations.add(location); //add them
 			}*/
-			if (getGrid().isValid(location.getAdjacentLocation(getLocation().getDirectionToward(location))) && getGrid().get(new Location(location.getRow(), location.getCol() + 1)) == null) {
-				if(getGrid().get(location) == null){
-					locations.add(new Location(location.getRow(), location.getCol() + 1)); //if the location 2 to the right is valid and is empty, add it
+			//&& getGrid().get(location.getAdjacentLocation(location.getDirectionToward(getLocation()) - 180)) == null)
+			if (getGrid().isValid(location.getAdjacentLocation(location.getDirectionToward(getLocation()) - 180))){
+				if(getGrid().get(location) == null){	
+					locations.add(location.getAdjacentLocation(location.getDirectionToward(getLocation()) - 180)); //if the location 2 to the right is valid and is empty, add it
 				}
 			}
-			if (getGrid().isValid(new Location(location.getRow(), location.getCol() - 1)) && getGrid().get(new Location(location.getRow(), location.getCol() - 1)) == null) {
+			/*if (getGrid().isValid(new Location(location.getRow(), location.getCol() - 1)) && getGrid().get(new Location(location.getRow(), location.getCol() - 1)) == null) {
 				if(getGrid().get(location) == null){
 					locations.add(new Location(location.getRow(), location.getCol() - 1)); //if the location 2 to the left is valid and empty, add it
 				}
-			}
+			}*/
 		}
 		return locations; //return the locations
 	}
