b9ab6c055f4f0f6de618fccb674db22d2f7dc6e1
TechnoD11 <technoD@outlook.com>
2015-05-30 12:11:13 -0400

    prog assign 13 updates

--- ProgramAssign13/src/Node.java
+++ ProgramAssign13/src/Node.java
@@ -1,5 +1,5 @@
 //<T> denotes that Node is a generic class (implicit parameters are of type object unless denoted with <> brackets when constructing new node)
-public class Node<T> {
+public class Node<T extends Comparable<T>> {
 
 	private T info; 
 	private Node<T> left, right;
@@ -10,25 +10,28 @@ public class Node<T> {
 		right = null;
 	}
 
-	public Node(T val, Node<T> link) {
-		info = val;
-		next = link;
-	}
-
 	public T getInfo() {
 		return info;
 	}
 
-	public Node<T> getNext() {
-		return next;
+	public Node<T> getLeft() {
+		return left;
+	}
+	
+	public Node<T> getRight() {
+		return right;
 	}
 
 	public void setInfo(T val) {
 		info = val;
 	}
 
-	public void setNext(Node<T> ptr) {
-		next = ptr;
+	public void setLeft(Node<T> ptr) {
+		left = ptr;
+	}
+	
+	public void setRight(Node<T> ptr) {
+		right = ptr;
 	}
 
 }
--- ProgramAssign13/src/Tree.java
+++ ProgramAssign13/src/Tree.java
@@ -1,4 +1,101 @@
-
-public class Tree {
-
+/**
+ * 
+ * @author Derek
+ * Programming Assignment 13
+ */
+public class Tree<T extends Comparable<T>> {
+	private Node<T> root;
+	private Node<T> current;
+	private Node<T> previous;
+	/**
+	 * 
+	 */
+	public Tree(){
+		root = null;
+		current = null;
+		previous = null;
+	}
+	/**
+	 * 
+	 * @param val
+	 */
+	public void delete(int val){
+		
+	}
+	/**
+	 * 
+	 */
+	private void deleteNoKids(){
+		
+	}
+	/**
+	 * 
+	 */
+	private void deleteOneKid(){
+		
+	}
+	/**
+	 * 
+	 */
+	private void deleteTwoKids(){
+		
+	}
+	/**
+	 * 
+	 * @param start
+	 */
+	private void doInorder(Node<Integer> start){
+		
+	}
+	/**
+	 * 
+	 * @param start
+	 */
+	private void doPostorder(Node<Integer> start){
+		
+	}
+	/**
+	 * 
+	 * @param start
+	 */
+	private void doPreorder(Node<Integer> start){
+		
+	}
+	/**
+	 * 
+	 * @param val
+	 */
+	private void find(int val){
+		
+	}
+	/**
+	 * maybe use private method addchild 
+	 * @param val
+	 */
+	public void insert(T val){
+		Node<T> newNode = new Node<T>(val);
+		if(root == null){
+			root = newNode;
+			return;
+		}
+		if()
+	}
+	/**
+	 * 
+	 */
+	public void inorder(){
+		
+	}
+	/**
+	 * 
+	 */
+	public void postorder(){
+		
+	}
+	/**
+	 * 
+	 */
+	public void preorder(){
+		
+	}
 }
